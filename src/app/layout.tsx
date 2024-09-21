ad6976eb6e27ead601165637ce5989e45e1089b3
JJONG\ljm17 <ljm920604@gmail.com>
2024-09-21 17:21:56 +0900

    Feat. 프로젝트 초기 세팅 + Tailwind className merge util함수

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import Header from "./components/Header";
+import { cn } from "./lib/utils";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +17,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "",
+          "font-sans flex flex-col w-full max-w-screen-2xl mx-auto"
+        )}
       >
-        {children}
+        <Header />
+        <main className="grow bg-sky-400">{children}</main>
       </body>
     </html>
   );
